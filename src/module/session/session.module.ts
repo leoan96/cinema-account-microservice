e7cfc1525ba3f5a9b4a5d49d62011489c4a9ba57
Mun Kit <50537292+leoan96@users.noreply.github.com>
2021-07-17 14:43:26 +0800

    use vault instead of hardcoding of kafka options (#21)

--- src/module/session/session.module.ts
+++ src/module/session/session.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientsModule, KafkaOptions, Transport } from '@nestjs/microservices';
+import { VAULT_CLIENT } from 'src/hashicorp-vault/vault.provider';
 import { AccountModule } from '../../module/account/account.module';
 import { RedisModule } from '../../module/redis/redis.module';
 import { API_ACCOUNT_KAFKA } from './redis-kafka-producer.constants';
@@ -8,23 +9,34 @@ import { RedisSubscribeExpiredService } from './redis-subscribe-expired.service'
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: API_ACCOUNT_KAFKA.SERVICE_NAME,
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: API_ACCOUNT_KAFKA.CLIENT_ID,
-            brokers: API_ACCOUNT_KAFKA.BROKERS,
-          },
-          send: {
-            acks: -1,
-          },
-          producer: {
-            idempotent: true,
-            maxInFlightRequests: 5,
-          },
+        useFactory: (vault): KafkaOptions => {
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: API_ACCOUNT_KAFKA.CLIENT_ID,
+                brokers: vault.KAFKA_BROKER.split(','),
+                ssl: true,
+                sasl: {
+                  mechanism: 'scram-sha-256',
+                  username: vault.KAFKA_USERNAME,
+                  password: vault.KAFKA_PASSWORD,
+                },
+              },
+              send: {
+                acks: -1,
+              },
+              // producer: {
+              //   idempotent: true,
+              //   maxInFlightRequests: 5,
+              // },
+            },
+          };
         },
+        inject: [VAULT_CLIENT],
       },
     ]),
     RedisModule,
